de1b6f06b5611569bcc2d4410384ed83a3337d5c
1codechic <1codechic@devchics.com>
2019-02-19 00:24:04 -0500

    initial commit

---
+++ store_item.rb
@@ -0,0 +1,7 @@
+tshirts = {:color => "white", :price => 12.95, :size => "medium"}
+jeans = {:color => "acid wash", :price => 54.95, :size => 12}
+shoes = {:color => "white", :price => 89.99, :size => 8}
+
+p "#{tshirts[:color]} #{tshirts[:price]} #{tshirts[:size]}"
+p "#{jeans[:color]} #{jeans[:price]} #{jeans[:size]}"
+p "#{shoes[:color]} #{shoes[:price]} #{shoes[:size]}"
