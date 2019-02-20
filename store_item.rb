2d8e0cbda49438214ad686776e8395ac553d4719
1codechic <1codechic@devchics.com>
2019-02-19 20:44:31 -0500

    refactored code from using hash to using a class. Added Getter methods

--- store_item.rb
+++ store_item.rb
@@ -1,7 +1,33 @@
-tshirts = {:color => "white", :price => 12.95, :size => "medium"}
-jeans = {:color => "acid wash", :price => 54.95, :size => 12}
-shoes = {:color => "white", :price => 89.99, :size => 8}
+# tshirts = {:color => "white", :price => 12.95, :size => "medium"}
+# jeans = {:color => "acid wash", :price => 54.95, :size => 12}
+# shoes = {:color => "white", :price => 89.99, :size => 8}
 
-p "#{tshirts[:color]} #{tshirts[:price]} #{tshirts[:size]}"
-p "#{jeans[:color]} #{jeans[:price]} #{jeans[:size]}"
-p "#{shoes[:color]} #{shoes[:price]} #{shoes[:size]}"
+class Shirt
+  def initialize(input_color, input_size, input_price)
+    @input_color = input_color
+    @input_size = input_size
+    @input_price = input_price
+  end
+
+# Getter Methods
+  def color
+    @input_color
+  end
+
+  def size
+    @input_size
+  end
+
+  def price
+    @input_price
+  end
+
+
+end
+
+shirt1 = Shirt.new("red", "medium", 25)
+p shirt1.color 
+p shirt1.size
+p shirt1.price
+
+
