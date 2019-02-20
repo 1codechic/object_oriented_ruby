1cd55f7569d7862cc598cc0df5ce6f821d4148e9
1codechic <1codechic@devchics.com>
2019-02-19 23:37:00 -0500

    refactored inheritance and fixed nil output

--- inheritance_example.rb
+++ inheritance_example.rb
@@ -1,38 +1,24 @@
 class Transportation
-  attr_reader :speed, :direction, :accelerate, :brake
-  attr_writer :turn
-
   def initialize
     @speed = 0
     @direction = 'north'
-    @accelerate = @speed += 10
-    @brake = @speed
+  end
+
+  def brake
+    @speed = 0
+  end
+
+  def accelerate
+    @speed += 10
+  end
+
+  def turn(new_direction)
+    @direction = new_direction
   end
 end
 
 
 class Car < Transportation
-  # attr_reader :speed, :direction, :accelerate, :brake
-  # attr_writer :turn
-
-  # def initialize
-  #   @speed = 0
-  #   @direction = 'north'
-  #   @accelerate = @speed += 10
-  #   @brake = @speed
-  # end
-
-  # def brake
-  #   @speed = 0
-  # end
-
-  # def accelerate
-  #   @speed += 10
-  # end
-
-  # def turn(new_direction)
-  #   @direction = new_direction
-  # end
 
   def honk_horn
     puts "Beeeeeeep!"
@@ -40,23 +26,7 @@ class Car < Transportation
 end
 
 class Bike < Transportation
-  # def initialize
-  #   @speed = 0
-  #   @direction = 'north'
-  # end
-
-  # def brake
-  #   @speed = 0
-  # end
-
-  # def accelerate
-  #   @speed += 10
-  # end
-
-  # def turn(new_direction)
-  #   @direction = new_direction
-  # end
-
+  
   def ring_bell
     puts "Ring ring!"
   end
@@ -65,18 +35,14 @@ end
 
 
 car1 = Car.new
+car1.accelerate
 p car1
-p car1.brake
+car1.honk_horn
 p car1
-p car1.accelerate
+car1.turn("west")
 p car1
-p car1.honk_horn
 
-# bike1 = Bike.new
-# p bike1
-# p bike1.ring_bell
-# p bike1
-# p bike1.accelerate
-# p bike1
-# p bike1.turn = "south"
-# p bike1
+bike1 = Bike.new
+bike1.ring_bell
+bike1.turn("east")
+p bike1
