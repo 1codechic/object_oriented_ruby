1907d8509346a86e11519953683be4d3410861e7
1codechic <1codechic@devchics.com>
2019-02-19 21:31:35 -0500

    removed commented getter methods

--- store_item.rb
+++ store_item.rb
@@ -4,35 +4,25 @@
 
 class Shirt
   attr_reader :input_color, :input_size, :input_price
-  attr_writer :input_color, :input_size
+  attr_writer :input_color, :input_size, :input_price
 
   def initialize(input_options)
     @input_color = input_options[:input_color]
     @input_size = input_options[:input_size]
     @input_price = input_options[:input_price]
   end
-
-# Getter Methods
-  # def color
-  #   @input_color
-  # end
-
-  # def size
-  #   @input_size
-  # end
-
-  # def price
-  #   @input_price
-  # end
-
-
 end
 
-shirt1 = Shirt.new(input_color: "red", input_size: "large", input_price: 30)
+shirt1 = Shirt.new(
+  input_color: "red", 
+  input_size: "large", 
+  input_price: 30)
+
 p shirt1.input_color 
 p shirt1.input_size
 p shirt1.input_price
 
 p shirt1.input_color = "blue"
-
+p shirt1.input_size = "small"
+p shirt1.input_price = 24.95
 
